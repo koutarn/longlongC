55cab9190373dfc3491b32754423f0ab0ed31013
koutarn <fuse.kanrinin@gmail.com>
2022-05-12 00:35:22 +0900

    initialize

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef long long longlongC;
+
+int main(int argc,char *argv[]){
+    longlongC long_long_c = 20;
+
+    printf("  /$$$$$$ \n");
+    printf(" /$$__  $$\n");
+    printf("| $$  \\__/\n");
+    printf("| $$      \n");
+    printf("| $$      \n");
+    printf("| $$    $$\n");
+    printf("|  $$$$$$/\n");
+    printf(" \\______/ \n");
+    printf("           \n");
+    printf("   /$$\n");
+
+    for(longlongC i = 0;i < long_long_c;i++){
+    printf("  | $$\n");
+    }
+    printf("  |_/  \n");
+
+
+
+    return 0;
+}
